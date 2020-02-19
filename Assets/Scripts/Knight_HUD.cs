e6049c106cdb15f990ff24a76c6fa0ea29213895
Atish Dhunnoo <atish-dhunnoo@hotmail.com>
2020-02-19 18:49:08 +0400

    Add Heart Textures // Start Health System Script // Add Heart UI

---
+++ Assets/Scripts/Knight_HUD.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Knight_HUD : MonoBehaviour
+{
+    public Sprite[] HeartSprites;
+    public Image HeartUI;
+    private Player player;
+
+
+}
