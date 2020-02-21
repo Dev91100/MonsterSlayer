145fbdf23e103d36ec958d5928200109f5c2bc2b
Atish Dhunnoo <atish-dhunnoo@hotmail.com>
2020-02-21 09:47:13 +0400

    Refine Knight_Combat Script // Add particle effect when attacking

--- Assets/Scripts/Knight_Script/Knight_Combat.cs
+++ Assets/Scripts/Knight_Script/Knight_Combat.cs
@@ -15,6 +15,11 @@ public class Knight_Combat : Knight_Movement
     bool die = false;
     public Rigidbody2D rb1;
 
+    //void Awake()
+    //{
+    //    rb1 = GetComponent<Rigidbody2D>();
+    //}
+
     //new attack
     //public LayerMask enemylayers;
     void Start()
@@ -65,52 +70,13 @@ public class Knight_Combat : Knight_Movement
         if (Input.GetButtonDown("Fire1"))
         {
             Attack();
-            createDust();
-        }
-
-        HandleInput();
-    }
-
-
-    void FixedUpdate()
-    {
-        //    Attack();
-    }
-
-    //Handles Keyboard Input for attacking animation
-    protected void HandleInput()
-    {
-        if (Input.GetButtonDown("Fire2"))
-        {
-            attack = true;
-            createDust();
-        }
-
-        if (Input.GetButtonUp("Fire2"))
-        {
-            attack = false;
-            createDust();
+            if (grounded)
+            {
+                createDust();
+            }
         }
     }
 
-
-    //void Attack()
-    //{
-    //    if (attack)
-    //    {
-    //        animator.SetTrigger("attack");
-    //    }
-
-
-    //    // Detect enemies in range of attack
-    //    Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-
-    //    //Damage enemies
-    //    foreach (Collider2D enemy in hitEnemies)
-    //    {
-    //        Debug.Log("We hit");
-    //    }
-    //}
 
     void Attack()
     {
@@ -144,8 +110,6 @@ public class Knight_Combat : Knight_Movement
 
 
     }
-
-
 
     void OnDrawGizmosSelected()
     {
