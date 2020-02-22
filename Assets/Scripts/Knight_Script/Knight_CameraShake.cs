bc8de473619b139639f6ffcfe7916850f2115191
Atish Dhunnoo <atish-dhunnoo@hotmail.com>
2020-02-22 06:46:57 +0400

    Complete Knight_CameraShake Script (Successful)

--- Assets/Scripts/Knight_Script/Knight_CameraShake.cs
+++ Assets/Scripts/Knight_Script/Knight_CameraShake.cs
@@ -3,33 +3,36 @@ using UnityEngine;
 
 public class Knight_CameraShake : MonoBehaviour
 {
-    private float shakeTimeRemaining, shakePower;
+    public static Knight_CameraShake instance;
 
-    public GameObject cineCamFind, cineCamActive, cineCamDeactive;
+    private float shakeTimeRemaining, shakePower, shakeFadeTime, shakeRotation;
+
+    public float rotationMultiplier;
 
     private CinemachineBrain cam;
     private Animator animator;
 
     void Start()
     {
+        instance = this;
         cam = GetComponent<CinemachineBrain>();
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            animator.enabled = false;
-            cam.enabled = false;
-            startShake(.1f, .3f);
-        }
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    animator.enabled = false;
+        //    cam.enabled = false;
+        //    startShake(.1f, .7f);
+        //}
 
-        if (Input.GetButtonUp("Fire1"))
-        {
-            animator.enabled = true;
-            cam.enabled = true;
-        }
+        //if (Input.GetButtonUp("Fire1"))
+        //{
+        //    animator.enabled = true;
+        //    cam.enabled = true;
+        //}
     }
 
     private void LateUpdate()
@@ -42,13 +45,25 @@ public class Knight_CameraShake : MonoBehaviour
             float yAmount = Random.Range(-1f, 1f) * shakePower;
 
             transform.position += new Vector3(xAmount, yAmount, 0f);
+
+            shakePower = Mathf.MoveTowards(shakePower, 0f, shakeFadeTime * Time.deltaTime);
+
+            shakeRotation = Mathf.MoveTowards(shakeRotation, 0f, shakeFadeTime * rotationMultiplier);
+
         }
+
+        transform.rotation = Quaternion.Euler(0f, 0f, shakeRotation * Random.Range(-1f, 1f));
+
     }
 
     public void startShake(float length, float power)
     {
         shakeTimeRemaining = length;
         shakePower = power;
+
+        shakeFadeTime = power / length;
+
+        shakeRotation = power * rotationMultiplier;
 
     }
 }
