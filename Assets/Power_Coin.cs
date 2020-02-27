2df4a1915fb061d59a9bda557672cc9c1f745f5f
Yuvir <yuvir.dessai@gmail.com>
2020-02-27 13:18:50 +0400

    Hierarchy Clean up and convert Power_Coin to prefab

--- Assets/Power_Coin.cs
+++ Assets/Power_Coin.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour
+public class Power_Coin : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
