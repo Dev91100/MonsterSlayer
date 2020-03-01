f49ff6b1538a0ba3552e9ed571ba788d9484c321
Atish Dhunnoo <atish-dhunnoo@hotmail.com>
2020-03-01 13:06:40 +0400

    Add Level_LoadingScreenCanvas Prefab // Add Level_LoadingScreen Script // Modify Level_SkipIntro scene

---
+++ Assets/Scripts/Level_Script/Level_LoadingScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_LoadingScreen : MonoBehaviour
+{
+    [SerializeField]
+    private float delayBeforeLoading = 2f;
+
+    [SerializeField]
+    private string sceneNameToLoad;
+
+    private float timeElapsed;
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+    }
+}
