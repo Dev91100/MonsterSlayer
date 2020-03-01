114075a02845ef61b6b1c770413b98e23e776f57
Atish Dhunnoo <atish-dhunnoo@hotmail.com>
2020-03-01 12:27:00 +0400

    Add Loading Screen between each scenes

--- Assets/Scripts/Level_Script/Level_SkipIntro.cs
+++ Assets/Scripts/Level_Script/Level_SkipIntro.cs
@@ -3,24 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class Level_SkipIntro : MonoBehaviour
 {
-    [SerializeField]
-    private float delayBeforeLoading = 39f;
-
-    [SerializeField]
-    private string sceneNameToLoad;
-
-    private float timeElapsed;
-
-    private void Update()
-    {
-        timeElapsed += Time.deltaTime;
-
-        if (timeElapsed > delayBeforeLoading)
-        {
-            SceneManager.LoadScene(sceneNameToLoad);
-        }
-    }
-
     public void skipIntro(string scenename)
     {
         SceneManager.LoadScene(scenename);
