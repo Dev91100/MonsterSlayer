a931fbe140108fec641626f46225f64a2faf529e
Atish Dhunnoo <atish-dhunnoo@hotmail.com>
2020-03-01 20:22:58 +0400

    Add comments to scripts

--- Assets/Scripts/Level_Script/Level_SceneSwitch.cs
+++ Assets/Scripts/Level_Script/Level_SceneSwitch.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// This script enables the scenes to be switched when the player gets to the end of a level
+
+// This script is attached to Level_SceneTrigger
+
 public class Level_SceneSwitch : MonoBehaviour
 {
     private int nextSceneToLoad;
 
     void Start()
     {
-        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1; // Gets the index of the next scene
     }
 
+
+    // Switch scene when the player gets in the trigger zone
 
     void OnTriggerEnter2D(Collider2D other)
     {
