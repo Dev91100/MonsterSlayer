70c4376c9493281a7b25710480cccdf20f947f88
Atish Dhunnoo <atish-dhunnoo@hotmail.com>
2020-03-02 10:03:53 +0400

    Add comments to script

--- Assets/Scripts/Menu_Script/Menu_PauseMenu.cs
+++ Assets/Scripts/Menu_Script/Menu_PauseMenu.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+
+// This script enables us to pause the game and bring up the pause menu
+
+// This script is attached to Level_PauseMenuCanvas
+
+/* 
+    Brackeys, 2017 : PAUSE MENU in Unity
+    Available from: https://www.youtube.com/watch?v=JivuXdrIHK0
+*/
 
 public class Menu_PauseMenu : MonoBehaviour
 {
-    public static bool gameIsPaused = false;
+    public static bool gameIsPaused = false; // Check if the game is paused or not
     public GameObject pauseMenuUI;
     private int currentScene;
 
@@ -14,9 +21,10 @@ public class Menu_PauseMenu : MonoBehaviour
         currentScene = SceneManager.GetActiveScene().buildIndex;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Check if escape key has been pressed down to enable or disable the pause menu
+
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             if (gameIsPaused)
@@ -30,25 +38,33 @@ public class Menu_PauseMenu : MonoBehaviour
         }
     }
 
-    public void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        gameIsPaused = false;
-    }
+    // This function enables the pause menu UI
 
     void Pause()
     {
         pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
+        Time.timeScale = 0f; // Freezes the game
         gameIsPaused = true;
     }
+
+    // This function resumes the game
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // Unfreezes the game
+        gameIsPaused = false;
+    }
+
+    // This function restarts the current scene
 
     public void Restart()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(currentScene);
     }
+
+    // This function starts the scene Menu_MainMenuScene
 
     public void LoadMenu()
     {
