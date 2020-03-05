ca929b94c4bcb3e6ffcff7773cf1c08345e3f4a6
Atish Dhunnoo <atish-dhunnoo@hotmail.com>
2020-03-05 07:48:44 +0400

    Add coin spawn to Barrel & Pot when destroyed

--- Assets/Scripts/Knight_Script/Knight_Barrel.cs
+++ Assets/Scripts/Knight_Script/Knight_Barrel.cs
@@ -7,12 +7,23 @@
 public class Knight_Barrel : MonoBehaviour
 {
     public Animator animator;
+    public Rigidbody2D[] rb2D;
+    public float thrust = 500f;
+    public GameObject[] loots;
 
     public void BreakBarrel()
     {
+        for (int i = 0; i < loots.Length; i++)
+        {
+            loots[i].SetActive(true);
+        }
+        for (int i = 0; i < loots.Length; i++)
+        {
+            rb2D[i].AddForce(transform.up * thrust);
+        }
+
         animator.SetTrigger("break"); //  Activates trigger for Barrel destruction animation
         Knight_SoundManager.PlaySound("Knight_Barrel"); // Activates sound for Barrel destruction
-
         Destroy(gameObject, .5f); // Destroys the game object from the scene
     }
 }
