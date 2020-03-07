9dc16d1cd4253f2ac0afd4c86a82c4d83fad458d
Atish Dhunnoo <atish-dhunnoo@hotmail.com>
2020-03-07 23:06:24 +0400

    Add fade in to Level_GameOver

---
+++ Assets/Scripts/Monster_Script/Monster_Loot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Monster_Loot : MonoBehaviour
+{
+    public Rigidbody2D[] rb2D;
+    public float thrust = 500f;
+    public GameObject[] loots;
+
+    public void monsterLoot(float xAmount, float yAmount)
+    {
+            Vector2 m_force = new Vector2(xAmount, yAmount) * thrust;
+            for (int i = 0; i < loots.Length; i++)
+            {
+                loots[i].SetActive(true);
+            }
+            for (int i = 0; i < loots.Length; i++)
+            {
+                rb2D[i].AddForce(m_force);
+            }
+    }
+}
