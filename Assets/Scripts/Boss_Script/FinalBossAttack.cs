ce05b1c202480cccbe10eeda506143c0a827007f
Atish Dhunnoo <atish-dhunnoo@hotmail.com>
2020-03-09 14:45:47 +0400

    Adjust Dragon collider in Level_Level3

---
+++ Assets/Scripts/Boss_Script/FinalBossAttack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBossAttack : MonoBehaviour
+{
+    public SpriteRenderer SR;
+    public Transform attackpt;
+    public float range = 0.5f;
+    public LayerMask player;
+    private float NextActionTime = 0.0f;
+    public float period = 1f;
+    public float length;
+    public float height;
+    private float depth;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        Collider2D[] hitplayer = Physics2D.OverlapCircleAll(attackpt.position, range, player);
+
+        foreach (Collider2D player in hitplayer)
+        {
+            if (Time.time > NextActionTime)
+            {
+                NextActionTime = Time.time + period;
+
+                player.GetComponent<Knight_Combat>().PlayerTakeDamage(1);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(attackpt.position, new Vector3(length, height, depth));
+    }
+}
---
+++ Assets/Scripts/Boss_Script/FinalBossHealth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalBossHealth : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator animator;
+    public int maxhealth = 100;
+    private int currenthealth = 0;
+    private bool die = false;
+
+    private int nextSceneToLoad;
+
+    private void Start()
+    {
+        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+        currenthealth = maxhealth;
+    }
+
+    public void MonsterTakeDamage(int damage)
+    {
+        currenthealth -= damage;
+
+        animator.SetTrigger("Hurt");
+
+        if (currenthealth <= 0)
+        {
+            animator.SetBool("IsDead", true);
+        }
+    }
+
+    public void DisableMonster()
+    {
+        SceneManager.LoadScene(nextSceneToLoad);
+        Destroy(gameObject);
+    }
+}
