11015dff0d1a93dc32b7719d20b8033fe9b03c7e
Mounthazeer <oudeenmounthazeer473@gmail.com>
2020-03-09 11:26:55 +0400

    Final monster health and damage with ai

--- Assets/Scripts/Boss_Script/Dragon_Fireball_Behaviour.cs
+++ Assets/Scripts/Boss_Script/Dragon_Fireball_Behaviour.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Dragon_Fireball_Behaviour : MonoBehaviour
 {
     public float Dragon_Fireball_speed;
     public float Dragon_Fireball_Lifetime;
-
     public GameObject destroyEffect;
     // Start is called before the first frame update
     private void Start()
@@ -17,16 +17,23 @@ public class Dragon_Fireball_Behaviour : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        transform.Translate(transform.right * Dragon_Fireball_speed * Time.deltaTime);
+
+
+        if (Dragon_Pathfinding.x >= 0.00f || Dragon_Pathfinding.x <= 0.00f)
+        {
+            transform.Translate(transform.up * Dragon_Fireball_speed * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0, -90);
+        }
+
     }
-    
+
     void DestroyProjectile()
     {
         //Instantiate(destroyEffect, transform.position, Quaternion.identity);
         //Destroy(gameObject);
-        Destroy(gameObject, 2f);
+        Destroy(gameObject, Dragon_Fireball_Lifetime);
     }
-    
+
     /*
     void OnTriggerEnter2D(Collider2D col)
     {
