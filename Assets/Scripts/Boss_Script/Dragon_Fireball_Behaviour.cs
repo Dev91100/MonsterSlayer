369f33e099a819e04aa3cc03ed0841bb9d6371cc
Atish Dhunnoo <atish-dhunnoo@hotmail.com>
2020-03-23 08:06:53 +0400

    Comment script Dragon_Fireball_Behaviour

--- Assets/Scripts/Boss_Script/Dragon_Fireball_Behaviour.cs
+++ Assets/Scripts/Boss_Script/Dragon_Fireball_Behaviour.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
 
+// This script controls the Fireball Object's direction, speed and lifetime 
+
+// This script is attached to Boss_FireBall
+
 public class Dragon_Fireball_Behaviour : MonoBehaviour
 {
     public float Dragon_Fireball_speed;
@@ -8,13 +12,15 @@ public class Dragon_Fireball_Behaviour : MonoBehaviour
 
     private void Update()
     {
+        // This if statement tests if the dragon is moving
         if (Dragon_Pathfinding.x >= 0.00f || Dragon_Pathfinding.x <= 0.00f)
         {
-            transform.Translate(transform.up * Dragon_Fireball_speed * Time.deltaTime);
-            transform.eulerAngles = new Vector3(0, 0, -90);
+            transform.Translate(transform.up * Dragon_Fireball_speed * Time.deltaTime); // Moves the fireball downwards
+            transform.eulerAngles = new Vector3(0, 0, -90); // Rotates the fireball to face downwards
         }
     }
 
+    // This function destroys the object Boss_FireBall
     void DestroyProjectile()
     {
         Destroy(gameObject, Dragon_Fireball_Lifetime);
